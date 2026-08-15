ecb7b679c5591ec8e7c8f74b7c383b3777f78a4d
empsf179it-tech <empsf179it@gmail.com>
2026-08-15 21:18:19 +0530

    third commit

--- check_divs.ps1
+++
@@ -1,8 +0,0 @@
-$files = @('register.html', 'home-2.html')
-foreach ($file in $files) {
-    $content = Get-Content $file -Raw
-    $openDivs = ([regex]::Matches($content, '<div\b')).Count
-    $closeDivs = ([regex]::Matches($content, '</div\b')).Count
-    $diff = $openDivs - $closeDivs
-    Write-Output "$file divs: +$openDivs -$closeDivs (diff: $diff)"
-}
